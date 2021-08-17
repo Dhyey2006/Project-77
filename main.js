08c5291b754bde535838a5a49e263ca39d670130
Dhyey2006 <85690437+Dhyey2006@users.noreply.github.com>
2021-08-17 21:04:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("My first console project");
+var first_name = "Dhyey";
+var last_name = "Panikar";
+var full_name =first_name.concat(last_name);
+console.log(first_name);
+
+var a=1;
+var b=2;
+
+var add=a+b;
+console.log(add);
+
+var sub=a-b;
+console.log(sub);
+
+var multiple=a*b;
+console.log(multiple);
+
+var divide=a/b;
+console.log(divide);
